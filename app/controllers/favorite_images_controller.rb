8c457dde43c69d971684340f8723222518f4306b
andresporras3423 <andresporras3423@gmail.com>
2020-10-02 16:28:42 -0500

    bring some images

--- app/controllers/favorite_images_controller.rb
+++ app/controllers/favorite_images_controller.rb
@@ -1,6 +1,13 @@
 class FavoriteImagesController < ApplicationController
   def index
-    
+    @search_name = params[:search_name]
+    unless @search_name.nil?
+        puts "WHAT A WONDERFUL WORLD"
+        url = "https://api.unsplash.com/search/photos?query=#{@search_name}&client_id=z88eko8yjR2DJRxJhpSWZAAS-eNdEP19QBE6mvQGrDI"
+        uri = URI(url)
+        response = Net::HTTP.get(uri)
+        @api_response = JSON.parse(response)["results"].map{|image| image["urls"]["small"]}
+    end
   end
 
   def show
