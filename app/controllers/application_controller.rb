934b76014734c55cfae8f0e05ac268244539cbaa
andresporras3423 <andresporras3423@gmail.com>
2020-10-04 19:48:51 -0500

    decorate view, index page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  #tutorial unsplash: https://github.com/unsplash/unsplash_rb/tree/7414642a7068a31cc872f5613bc3baff4a1b9428
   Unsplash.configure do |config|
       config.application_access_key = "z88eko8yjR2DJRxJhpSWZAAS-eNdEP19QBE6mvQGrDI"
       config.application_secret = "dWAWqJm_1kPwl9VOh8O-07c5_w2ortBj0VSsrNBS9O4"
